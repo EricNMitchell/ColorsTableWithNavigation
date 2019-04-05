6574b5d62da6e2159777803e797044d7dac6912e
Eric <emitchell008@gmail.com>
2019-04-05 18:54:56 -0500

    implemented functionality according to the challenge requirements

---
+++ ColorsTableView/ColorsTableView/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  ColorsTableView
+//
+//  Created by Eric Mitchell on 4/5/19.
+//  Copyright © 2019 Eric Mitchell. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
